7cf50c1c5ce8b610978f9ba2fe9125dd3752bc7c
Kesnel <kesnelsamuel@gmail.com>
2021-08-15 08:27:56 -0400

    Latest queries

--- sql/sql_query_kesnel_samuel_jean_philippe.sql
+++ sql/sql_query_kesnel_samuel_jean_philippe.sql
@@ -1,19 +1,19 @@
 -- 1. Calculate the number of health facilities per commune.
 SELECT 
 	COUNT(facdesc) as "# health facilities", 
-	vilcomn
+	vilcomn as "Commune"
 FROM spa
 	GROUP BY vilcomn
-	ORDER BY vilcomn
+	ORDER BY vilcomn;
 
 -- 2. Calculate the number of health facilities by commune and by type of health facility.
 SELECT 
 	COUNT(facdesc) as "# health facilities",
-	vilcomn,
-	facdesc_1
+	vilcomn as "Commune",
+	facdesc_1 as "Type"
 FROM spa
 	GROUP BY vilcomn, facdesc_1
-	ORDER BY vilcomn
+	ORDER BY facdesc_1;
 
 -- 3. Calculate the number of health facilities by municipality and by department.
 SELECT 
@@ -22,22 +22,16 @@ SELECT
 	departn
 FROM spa
 	GROUP BY vilcomn, departn
-	ORDER BY vilcomn
+	ORDER BY departn;
 
 -- 4. Calculate the number of sites by type (mga) and by department.
 SELECT 
 	COUNT(facdesc) as "Number Sites", 
-	mga as "Type (MGA)",
 	departn as "Department",
-CASE
-	WHEN mga=1 THEN 'Public'
-	WHEN mga=2 THEN 'Private non profit'
-	WHEN mga=3 THEN 'Private with profit'
-	WHEN mga=4 THEN 'Private and public'
-END as "Type (MGA)"
+	mga as "Type (MGA)"
 FROM spa
 	GROUP BY mga, departn
-	ORDER BY departn
+	ORDER BY mga;
 	
 -- 5. Calculate the number of sites with an ambulance by municipality and by department (ambulance = 1.0).
 SELECT 
@@ -47,17 +41,29 @@ SELECT
 FROM spa
 	WHERE ambulance = 1.0
 	GROUP BY departn, vilcomn
-	ORDER BY departn
+	ORDER BY departn;
 
 -- 6. Calculate the number of hospitals per 10k inhabitants per department.
 
+
 -- 7. Calculate the number of sites per 10k inhabitants per department * Question 9.
+
 
 -- 8. Calculate the number of bed per 1k inhabitants per department.
 
+
 -- 9. How many communes have fewer dispensaries than hospitals?
 
+
 -- 10. How many deaths per month?
+SELECT 
+	EXTRACT(YEAR FROM to_date(document_date,'YYYY-MM-DD')) AS Year,
+	TO_CHAR(to_date(document_date,'YYYY-MM-DD'), 'Month') AS Month,
+	SUM(deces) as "Death Rate"
+FROM covid_cases
+GROUP BY
+	Month,
+	Year;
 
 -- 11. How many prevalences per month?
 
